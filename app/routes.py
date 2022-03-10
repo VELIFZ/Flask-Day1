d61a85dcee075bae70e8531d519ae23e81e17a0c
VELIFZ <eliferik72@gmail.com>
2022-03-10 13:25:58 -0500

    jinjabasics

--- app/routes.py
+++ app/routes.py
@@ -16,18 +16,22 @@ from app import app # import the app variable defined in __init__.py
    # if your route's job is to display an htmk page -> it's return value should be a call to render_template
 from flask import render_template
 
+# # import choice for our random behaviour
+from random import choice # index kisminda x yerine random isimler atayacaz sayfa her yenilendiginde
+
 @app.route('/') # decorator says: this function is a route of the flask app 'app' with the url endpoint '/' -- /homepage olsaydi user cagirirken bunu kullanacakti endpoint olarak
 def home():
-    print('Merhaba!')
-    x = 5
-    print(f'X has value {5}. This will print in the terminal when this function runs when the url endpoint / is accessed.')
-    return render_template('index.html')
+    print('Hello, Foxes!')
+    x = choice(['Zambak', 'ELif', 'Foxy', 'Lilo'])
+    print(f'X has value {x}. This value will be passed into render_template as a keyword argument and the keyword used in a jinja expression in index.html')
+    return render_template('index.html', name=x)
 
 # bunlari yaptiktan sonra gidip __init__ kisminda app'e route kisminda html'i app'e bagladik - " from . import routes"
 
 # a second route!
 @app.route('/mcfc')
 def mancity():
-    x = 'Bir'
-    print(f'X has value {x}.')
-    return render_template('soccer.html')
+    headline = 'Futbolla ilgili sacmalaliklar'
+    mcgoals = '5-0'
+    sportinggoals = 0 
+    return render_template('soccer.html', headline= headline, mcfcg=mcgoals, slg=sportinggoals)
